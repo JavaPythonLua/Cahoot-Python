94ef3477eb52d07650935fff723f12dd41e436a6
JavaPythonLua <alex_inns@outlook.com>
2026-08-06 17:23:58 +0100

    Update licensing

--- serverSQL.py
+++ serverSQL.py
@@ -1,6 +1,15 @@
 #"serverSQL.py" A program to try to re-create the popular quiz game Kahoot! in python
 #Copyright (C) 2026  Alex Inns
 
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU Affero General Public License version 3, as
+#published by the Free Software Foundation.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU Affero General Public License for more details.
+
 #You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
